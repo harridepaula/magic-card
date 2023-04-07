ecacfef2c24e4d0799dc0fe3526e1ce09adbe3ed
Harri <harri.kinsman@gmail.com>
2023-04-07 18:32:24 +0100

    Added color theme button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// quote API
 let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
@@ -17,4 +18,27 @@ let getQuote = () => {
 
 btn.addEventListener("click", getQuote);
 
-// window.addEventListener("load", getQuote);
+// change theme
+const colorBtn = document.querySelector("#color-btn");
+const container = document.querySelector(".container");
+
+colorBtn.addEventListener("click", function () {
+  if (container.classList.contains("dark-mode")) {
+    container.classList.remove("dark-mode");
+    container.classList.add("light-mode");
+  } else if (container.classList.contains("light-mode")) {
+    container.classList.remove("light-mode");
+    container.classList.add("purple-mode");
+  } else if (container.classList.contains("purple-mode")) {
+    container.classList.remove("purple-mode");
+    container.classList.add("star-mode");
+  } 
+  else if (container.classList.contains("star-mode")) {
+    container.classList.remove("star-mode");
+    container.classList.add("blue-mode");
+  }
+  else {
+    container.classList.remove("blue-mode");
+    container.classList.add("dark-mode");
+  }
+});
